25c572101c7b93ab91331e09245d98f355ffb8bb
BogomolovDmitry <31962802+BogomolovDmitry@users.noreply.github.com>
2017-11-22 22:41:36 +0300

    Update ctxmngrs.py

--- librip/ctxmngrs.py
+++ librip/ctxmngrs.py
@@ -6,3 +6,25 @@
 #   sleep(5.5)
 #
 # После завершения блока должно вывестись в консоль примерно 5.5
+import datetime
+import contextlib
+
+
+@contextlib.contextmanager
+def timer2():
+    t1 = datetime.datetime.now()
+    yield
+    t2 = datetime.datetime.now()
+    res = t2 - t1
+    res1 = str(res.seconds) + '.' + str(res.microseconds)
+    print('Execution time {0}'.format(res1))
+
+
+class timer:
+    def __enter__(self):
+        self.t1 = datetime.datetime.now()
+
+    def __exit__(self, exp_type, exp_value, traceback):
+        t2 = datetime.datetime.now()
+        res = t2 - self.t1
+        print('Execution time {0}'.format(res))
