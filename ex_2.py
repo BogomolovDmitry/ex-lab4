b695238913a1cf60403bacad64789f0aa83e8a56
BogomolovDmitry <31962802+BogomolovDmitry@users.noreply.github.com>
2017-11-22 22:36:10 +0300

    Update ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 from librip.gens import gen_random
 from librip.iterators import Unique
 
@@ -6,3 +5,7 @@ data1 = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
 data2 = gen_random(1, 3, 10)
 
 # Реализация задания 2
+print(*Unique(data1), sep=", ")
+print(*Unique(data2), sep=", ")
+print(*Unique(['A','a','B','b']), sep=", ")
+print(*Unique(['A','a','B','b'], ignore_case = True), sep=", ")
